05c292937bc86e89c10cb75f33deede86c74ad95
Sergey Protasov <69373636+st43r@users.noreply.github.com>
2023-06-21 17:09:30 +0300

    Add files via upload

---
+++ CurveLibrary/Validation.h
@@ -0,0 +1,12 @@
+#ifndef _validation_h_
+#define _validation_h_
+
+
+/// <summary>
+/// Checks whether <paramref name="Length"/> is valid.
+/// </summary>
+/// <param name="length">Length value.</param>
+extern void CheckLength(double length);
+
+
+#endif _validation_h_
